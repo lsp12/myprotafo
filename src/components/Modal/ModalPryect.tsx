7ce12c31ed59cbf8b43748b38d6715a8ceb1b495
lsp12 <jonathankenny852@gmail.com>
2021-11-05 22:51:41 -0500

    friendly design

--- src/components/Modal/ModalPryect.tsx
+++ src/components/Modal/ModalPryect.tsx
@@ -1,93 +1,104 @@
-import * as React from 'react';
-import Backdrop from '@mui/material/Backdrop';
-import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
-import Fade from '@mui/material/Fade';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { IFormTechonology, IProject } from '../../interface/interface';
-import CreateIcon from '@mui/icons-material/Create';
-import { Card, CardActions, CardContent, Chip, FormControl, InputLabel, MenuItem, OutlinedInput, Select, SelectChangeEvent, TextField } from '@mui/material';
-import { useAppDispatch, useAppSelector } from '../../Store/hooks';
-import { useFormik } from 'formik';
-import { FormWebPageSchema } from '../../validationSchema/validationSchema';
-import { toast } from 'react-toastify';
+import * as React from "react";
+import Backdrop from "@mui/material/Backdrop";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { IFormTechonology, IProject } from "../../interface/interface";
+import CreateIcon from "@mui/icons-material/Create";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  Chip,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  OutlinedInput,
+  Select,
+  SelectChangeEvent,
+  TextField,
+} from "@mui/material";
+import { useAppDispatch, useAppSelector } from "../../Store/hooks";
+import { useFormik } from "formik";
+import { FormWebPageSchema } from "../../validationSchema/validationSchema";
+import { toast } from "react-toastify";
 import { Theme, useTheme } from "@mui/material/styles";
-import { useEffect } from 'react';
-import { updateProject } from '../../Store/ActionWebPage/WebPageReducer';
+import { useEffect } from "react";
+import { updateProject } from "../../Store/ActionWebPage/WebPageReducer";
 
 const style = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
+  bgcolor: "background.paper",
+  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
 };
 
 interface Iproyectprops {
-    proyect: IProject;
-  }
+  proyect: IProject;
+}
 
-  interface IFormWebPage {
-    title: "";
-    description: "";
-    link: "";
-    item: [];
-  }
-  
-  const ITEM_HEIGHT = 48;
-  const ITEM_PADDING_TOP = 8;
-  const MenuProps = {
-    PaperProps: {
-      style: {
-        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-        width: 250,
-      },
+interface IFormWebPage {
+  title: "";
+  description: "";
+  link: "";
+  item: [];
+}
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
     },
-  };
-  
-  function getStyles(name: string, personName: readonly string[], theme: Theme) {
-    return {
-      fontWeight:
-        personName.indexOf(name) === -1
-          ? theme.typography.fontWeightRegular
-          : theme.typography.fontWeightMedium,
-    };
-  }
+  },
+};
 
-export default function ProyectTransitionsModal({proyect}: Iproyectprops) {
+function getStyles(name: string, personName: readonly string[], theme: Theme) {
+  return {
+    fontWeight:
+      personName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
+
+export default function ProyectTransitionsModal({ proyect }: Iproyectprops) {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
   const theme = useTheme();
   const [personName, setPersonName] = React.useState<string[]>([]);
-  const items= useAppSelector((state) => state.technology.technology);
+  const items = useAppSelector((state) => state.technology.technology);
   const dispatch = useAppDispatch();
 
-    useEffect(() => {
-        const tecnologiesIdsToPut: any = [];
-        const item = proyect.item;
-        if(item !== undefined && item.length > 0){
-            item.map((e:any) => {
-                tecnologiesIdsToPut.push(e);
-            });
-        }
-        setPersonName(tecnologiesIdsToPut as string[]);
-        
-    }, [items]);
+  useEffect(() => {
+    const tecnologiesIdsToPut: any = [];
+    const item = proyect.item;
+    if (item !== undefined && item.length > 0) {
+      item.map((e: any) => {
+        tecnologiesIdsToPut.push(e);
+      });
+    }
+    setPersonName(tecnologiesIdsToPut as string[]);
+  }, [items]);
 
   function nameById(data: string) {
     const tecnology = items.filter(
       (tecnology: IFormTechonology) => tecnology.id === data
     );
-    if(tecnology.length > 0){
-        return tecnology[0].technology;
+    if (tecnology.length > 0) {
+      return tecnology[0].technology;
     }
-    
+
     return "";
   }
 
@@ -112,13 +123,13 @@ export default function ProyectTransitionsModal({proyect}: Iproyectprops) {
       validationSchema: FormWebPageSchema,
       onSubmit: async ({ title, description, link, item }) => {
         try {
-            const id = proyect.id;
-            console.log("id del proyecto a actualizar"+id);
-          const logued = {id ,title, description, link, item: personName };
+          const id = proyect.id;
+          console.log("id del proyecto a actualizar" + id);
+          const logued = { id, title, description, link, item: personName };
           await dispatch(updateProject(logued));
           console.log(logued);
           setPersonName([]);
-          setOpen(false)
+          setOpen(false);
           resetForm();
         } catch (error) {
           toast.error("Algo fallo..?");
@@ -127,11 +138,9 @@ export default function ProyectTransitionsModal({proyect}: Iproyectprops) {
       },
     });
 
-
-
   return (
     <div>
-      <CreateIcon onClick={handleOpen} sx={{cursor:"pointer"}} />
+      <CreateIcon onClick={handleOpen} sx={{ cursor: "pointer" }} />
       <Modal
         aria-labelledby="transition-modal-title"
         aria-describedby="transition-modal-description"
@@ -145,127 +154,140 @@ export default function ProyectTransitionsModal({proyect}: Iproyectprops) {
       >
         <Fade in={open}>
           <Box sx={style}>
-            
-
-          <form onSubmit={handleSubmit} autoComplete="off">
-      <Card
-        sx={{
-          width: "100%",
-          
-        }}
-      >
-        <CardContent>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            Application form new web page
-          </Typography>
-          <Typography
-            variant="h5"
-            component="div"
-            marginTop="0.5em"
-            textAlign="center"
-          >
-            Update web page
-          </Typography>
-          <Typography
-            sx={{ mb: 1.5, marginTop: "2em", textAlign: "center" }}
-            color="text.secondary"
-          >
-            <TextField
-              id="outlined-basic"
-              label="Name the Project"
-              variant="outlined"
-              color="secondary"
-              {...getFieldProps("title")}
-              sx={{ width: "100%" }}
-            />
-          </Typography>
-          {errors.title && touched.title && (
-            <Typography sx={{ fontWeight: 700, fontSize: 14, color: "red" }}>
-              {errors.title}
-            </Typography>
-          )}
-
-          <Typography
-            sx={{ mb: 1.5, marginTop: "2em", textAlign: "center" }}
-            color="text.secondary"
-          >
-            <TextField
-              id="outlined-basic"
-              label="Link the image"
-              variant="outlined"
-              color="secondary"
-              {...getFieldProps("link")}
-              sx={{ width: "100%" }}
-            />
-            {errors.link && touched.link && (
-              <Typography sx={{ fontWeight: 700, fontSize: 14, color: "red" }}>
-                {errors.link}
-              </Typography>
-            )}
-          </Typography>
-
-          <Typography
-            sx={{ mb: 1.5, marginTop: "2em", textAlign: "center" }}
-            color="text.secondary"
-          >
-            <TextField
-              id="outlined-basic"
-              label="Description"
-              variant="outlined"
-              multiline
-              rows={4}
-              color="secondary"
-              {...getFieldProps("description")}
-              sx={{ width: "100%" }}
-            />
-            {errors.description && touched.description && (
-              <Typography sx={{ fontWeight: 700, fontSize: 14, color: "red" }}>
-                {errors.description}
-              </Typography>
-            )}
-          </Typography>
-
-          <FormControl sx={{ m: 1, width: 300 }} variant="filled">
-            <InputLabel id="demo-multiple-chip-label">Chip</InputLabel>
-            <Select
-              color="primary"
-              labelId="demo-multiple-chip-label"
-              id="demo-multiple-chip"
-              multiple
-              value={personName}
-              onChange={handleChange}
-              input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
-              renderValue={(selected) => (
-                <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
-                  {(selected as string[]).map((value) => (
-                    <Chip key={value} label={nameById(value)} />
-                  ))}
-                </Box>
-              )}
-              MenuProps={MenuProps}
-            >
-              {items.length > 0 &&
-                items.map((name: IFormTechonology) => (
-                  <MenuItem
-                    key={name.id}
-                    value={name.id}
-                    style={getStyles(name.technology, personName, theme)}
+            <form onSubmit={handleSubmit} autoComplete="off">
+              <Card
+                sx={{
+                  width: "100%",
+                }}
+              >
+                <CardContent>
+                  <Typography
+                    sx={{ fontSize: 14 }}
+                    color="text.secondary"
+                    gutterBottom
                   >
-                    {name.technology}
-                  </MenuItem>
-                ))}
-            </Select>
-          </FormControl>
-        </CardContent>
-        <CardActions sx={{ justifyContent: "center" }}>
-          <Button size="small" color="primary" type="submit">
-            Save
-          </Button>
-        </CardActions>
-      </Card>
-    </form>
+                    Application form new web page
+                  </Typography>
+                  <Typography
+                    variant="h5"
+                    component="div"
+                    marginTop="0.5em"
+                    textAlign="center"
+                  >
+                    Update web page
+                  </Typography>
+                  <Typography
+                    sx={{ mb: 1.5, marginTop: "2em", textAlign: "center" }}
+                    color="text.secondary"
+                  >
+                    <TextField
+                      id="outlined-basic"
+                      label="Name the Project"
+                      variant="outlined"
+                      color="secondary"
+                      {...getFieldProps("title")}
+                      sx={{ width: "100%" }}
+                    />
+                  </Typography>
+                  {errors.title && touched.title && (
+                    <Typography
+                      sx={{ fontWeight: 700, fontSize: 14, color: "red" }}
+                    >
+                      {errors.title}
+                    </Typography>
+                  )}
 
+                  <Typography
+                    sx={{ mb: 1.5, marginTop: "2em", textAlign: "center" }}
+                    color="text.secondary"
+                  >
+                    <TextField
+                      id="outlined-basic"
+                      label="Link the image"
+                      variant="outlined"
+                      color="secondary"
+                      {...getFieldProps("link")}
+                      sx={{ width: "100%" }}
+                    />
+                    {errors.link && touched.link && (
+                      <Typography
+                        sx={{ fontWeight: 700, fontSize: 14, color: "red" }}
+                      >
+                        {errors.link}
+                      </Typography>
+                    )}
+                  </Typography>
 
+                  <Typography
+                    sx={{ mb: 1.5, marginTop: "2em", textAlign: "center" }}
+                    color="text.secondary"
+                  >
+                    <TextField
+                      id="outlined-basic"
+                      label="Description"
+                      variant="outlined"
+                      multiline
+                      rows={4}
+                      color="secondary"
+                      {...getFieldProps("description")}
+                      sx={{ width: "100%" }}
+                    />
+                    {errors.description && touched.description && (
+                      <Typography
+                        sx={{ fontWeight: 700, fontSize: 14, color: "red" }}
+                      >
+                        {errors.description}
+                      </Typography>
+                    )}
+                  </Typography>
+
+                  <FormControl sx={{ m: 1, width: 300 }} variant="filled">
+                    <InputLabel id="demo-multiple-chip-label">Chip</InputLabel>
+                    <Select
+                      color="primary"
+                      labelId="demo-multiple-chip-label"
+                      id="demo-multiple-chip"
+                      multiple
+                      value={personName}
+                      onChange={handleChange}
+                      input={
+                        <OutlinedInput id="select-multiple-chip" label="Chip" />
+                      }
+                      renderValue={(selected) => (
+                        <Box
+                          sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}
+                        >
+                          {(selected as string[]).map((value) => (
+                            <Chip key={value} label={nameById(value)} />
+                          ))}
+                        </Box>
+                      )}
+                      MenuProps={MenuProps}
+                    >
+                      {items.length > 0 &&
+                        items.map((name: IFormTechonology) => (
+                          <MenuItem
+                            key={name.id}
+                            value={name.id}
+                            style={getStyles(
+                              name.technology,
+                              personName,
+                              theme
+                            )}
+                          >
+                            {name.technology}
+                          </MenuItem>
+                        ))}
+                    </Select>
+                  </FormControl>
+                </CardContent>
+                <CardActions sx={{ justifyContent: "center" }}>
+                  <Button size="small" color="secondary" type="submit">
+                    Save
+                  </Button>
+                </CardActions>
+              </Card>
+            </form>
           </Box>
         </Fade>
       </Modal>
